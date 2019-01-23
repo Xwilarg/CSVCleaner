529f45f52886f83fdd305b66e6d3e3cc3fe7727e
Xwilarg <xwilarg@yahoo.fr>
2019-01-23 01:14:36 +0100

    Add ability to open files

--- CSVCleaner/MainWindow.cpp
+++ CSVCleaner/MainWindow.cpp
@@ -1,14 +1,40 @@
+#include <QFileDialog>
+#include <QMessageBox>
+#include <memory>
 #include "MainWindow.hpp"
 
 namespace CSVCleaner
 {
     MainWindow::MainWindow() noexcept
-        : QMainWindow(nullptr)
+        : QMainWindow(nullptr), _fileMenu(menuBar()->addMenu(tr("File"))), _helpMenu(menuBar()->addMenu(tr("?"))),
+          _openAction(std::make_unique<QAction>(tr("Open"), this)), _quitAction(std::make_unique<QAction>(tr("Quit"), this)),
+          _aboutQtAction(std::make_unique<QAction>(tr("About Qt"), this))
     {
+        setWindowTitle("CSVCleaner");
+        _openAction->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_O));
+        connect(_openAction.get(), SIGNAL(triggered()), this, SLOT(OpenFile()));
+        connect(_quitAction.get(), SIGNAL(triggered()), this, SLOT(ExitApp()));
+        connect(_aboutQtAction.get(), SIGNAL(triggered()), this, SLOT(AboutQt()));
+        _fileMenu->addAction(_openAction.get());
+        _fileMenu->addAction(_quitAction.get());
+        _helpMenu->addAction(_aboutQtAction.get());
     }
 
-    MainWindow::~MainWindow() noexcept
+    void MainWindow::OpenFile()
     {
+        QFile file(QFileDialog::getOpenFileName(this, tr("Open CSV"), ".", "CSV (*.csv);;Other(*)"));
+        file.open(QIODevice::ReadOnly);
+        QString content(file.readAll());
+        file.close();
+    }
 
+    void MainWindow::ExitApp() const noexcept
+    {
+        std::exit(0);
+    }
+
+    void MainWindow::AboutQt() const noexcept
+    {
+        QMessageBox::aboutQt(nullptr);
     }
 }
--- CSVCleaner/MainWindow.hpp
+++ CSVCleaner/MainWindow.hpp
@@ -2,6 +2,8 @@
 # define MAINWINDOW_HPP
 
 # include <QMainWindow>
+# include <QMenuBar>
+# include <memory>
 
 namespace CSVCleaner
 {
@@ -11,7 +13,16 @@ namespace CSVCleaner
 
     public:
         MainWindow() noexcept;
-        ~MainWindow() noexcept;
+        ~MainWindow() noexcept = default;
+
+    private:
+        QMenu *_fileMenu, *_helpMenu;
+        std::unique_ptr<QAction> _openAction, _quitAction, _aboutQtAction;
+
+    private slots:
+        void OpenFile();
+        __attribute__((noreturn)) void ExitApp() const noexcept;
+        void AboutQt() const noexcept;
     };
 }
 
